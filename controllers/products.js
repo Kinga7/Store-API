2990bcb5344c4c08bf59a23ec3f1d208de676bd3
Austin King <awkscbw@gmail.com>
2022-09-14 06:12:34 -0700

    more search functionality

--- controllers/products.js
+++ controllers/products.js
@@ -3,14 +3,16 @@ const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res) => {
     const search = 'a'
-    const products = await Product.find({
-        name:{$regex:search, $options: 'i'},
-    })
+    const products = await Product.find({})
+    .sort('name')
+    .select('name price')
+    .limit(10)
+    .skip(1)
     res.status(200).json({products, nbHits: products.length})
 }
 
 const getAllProducts= async (req, res) => {
-    const {featured, company, name} = req.query
+    const {featured, company, name, sort, fields} = req.query
 const queryObject = {}
 
 if(featured){
@@ -20,11 +22,34 @@ if(company){
     queryObject.company = company
 }
 if(name){
-    queryObject.name = name
+    queryObject.name = {$regex:name, $options: 'i'}
 }
-console.log(queryObject)
+//console.log(queryObject)
 
-    const products = await Product.find(queryObject) 
+let result = Product.find(queryObject)
+// sort
+if(sort){
+       const sortList = sort.split(',').join(' ')
+       result = result.sort(sortList)
+    }
+else{
+        result = result.sort('createdAt')
+    }
+if(fields){
+        const fieldsList = fields.split(',').join(' ')
+        result = result.select(fieldsList)
+    }
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10
+    const skip = (page-1) * limit;
+
+    result = result.skip(skip).limit(limit)
+//23 products
+//4 7 7 7 2
+
+
+
+    const products = await result
     res.status(200).json({products, nbHits: products.length})
 }
 
